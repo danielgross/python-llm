420cb76e3df4fb5e2edc72c74acf1cc4c260f46a
danielgross <d@dcgross.com>
2023-11-29 20:16:25 -0800

    add pplx

--- llm.py
+++ llm.py
@@ -7,27 +7,17 @@ import asyncio
 import os
 import shelve
 import functools
+import aiohttp
 from tenacity import retry, stop_after_attempt
 _env_dir = os.path.expanduser('~/.env')if \
     os.path.isfile(os.path.expanduser('~/.env')) else None
 dotenv.load_dotenv(_env_dir)
 _cache_file = "model_cache"
 
-_live_requests = {}
-def log(func):
-    @functools.wraps(func)
-    async def wrapper(*args, **kwargs):
-        global _live_requests
-        _live_requests[func.__name__] = _live_requests.get(func.__name__, 0) + 1
-        print(_live_requests)
-        result = await func(*args, **kwargs)
-        _live_requests[func.__name__] -= 1
-        print(_live_requests)
-        return result
-    return wrapper
 
 def limit_concurrency(max_concurrent_tasks):
     semaphore = asyncio.Semaphore(max_concurrent_tasks)
+
     def decorator(func):
         @functools.wraps(func)
         async def wrapper(*args, **kwargs):
@@ -36,9 +26,9 @@ def limit_concurrency(max_concurrent_tasks):
         return wrapper
     return decorator
 
+
 @limit_concurrency(10)
 @retry(stop=stop_after_attempt(3))
-@log
 async def replicate_request(input, model='mistral'):
     model_urls = {
         'mistral-7b': 'mistralai/mistral-7b-instruct-v0.1:83b6a56e7c828e667f21fd596c338fd4f0039b46bcfa18d973e8e70e455fda70'
@@ -50,9 +40,9 @@ async def replicate_request(input, model='mistral'):
     result = ''.join(output)
     return result
 
+
 @limit_concurrency(10)
 @retry(stop=stop_after_attempt(3))
-@log
 async def openai_request(input, model="gpt-3.5-turbo"):
     completion = await openai.ChatCompletion.acreate(
         model=model,
@@ -63,21 +53,47 @@ async def openai_request(input, model="gpt-3.5-turbo"):
     result = completion.choices[0].message.content
     return result
 
+
 @limit_concurrency(2)
 @retry(stop=stop_after_attempt(3))
-@log
 async def anthropic_request(input, model="claude-2"):
     client = anthropic.AsyncAnthropic()
     completion = await client.completions.create(
-    model=model,
-    max_tokens_to_sample=50,
+        model=model,
+        max_tokens_to_sample=50,
         prompt=f"{anthropic.HUMAN_PROMPT} {input}{anthropic.AI_PROMPT}",
     )
 
     return completion.completion
 
+@retry(stop=stop_after_attempt(3))
+async def pplx_request(input, model="pplx-7b-online"):
+    url = "https://api.perplexity.ai/chat/completions"
+    token = os.getenv('PPLX_API_KEY')
+    if token is None:
+        raise ValueError("PPLX_API_KEY environment variable is not set.")
+    payload = {
+        "model": model,
+        "messages": [
+            {
+                "role": "user",
+                "content": input
+            }
+        ]
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}"
+    }
 
-async def complete(inputs, models=None, use_cache=False):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=payload, headers=headers) as response:
+            result = await response.json()
+            return result['choices'][0]['message']['content']
+
+
+async def complete(inputs, models=None, use_cache=False, concurrency_limit=10):
     if models is None:
         models = ["openai/gpt-3.5-turbo"]  # Default model if none specified
 
@@ -98,13 +114,15 @@ async def complete(inputs, models=None, use_cache=False):
                     continue
 
                 provider, model_name = model.split('/', 1)
-                
+
                 if provider == 'replicate':
                     task = replicate_request(input, model=model_name)
                 elif provider == 'openai':
                     task = openai_request(input, model=model_name)
                 elif provider == 'anthropic':
                     task = anthropic_request(input, model=model_name)
+                elif provider == 'pplx':
+                    task = pplx_request(input, model=model_name)
                 else:
                     raise ValueError(f"Unknown provider: {provider}")
 
@@ -133,11 +151,14 @@ async def complete(inputs, models=None, use_cache=False):
 def test():
     import json
     test_prompts = ["what model are you?",
-                    "what is 2+2?", "what is the 9th digit of pi?"]
+                    "what is 2+2?", "what is the current date?"]
     models = [
         'openai/gpt-3.5-turbo',
+        'anthropic/claude-2',
         'replicate/mistral-7b',
-        'anthropic/claude-2'
+        'pplx/pplx-70b-online',
+        
+
     ]
     # Call the complete function with the test prompts
     responses = asyncio.run(
@@ -145,6 +166,7 @@ def test():
 
     # Print the responses
     print(json.dumps(responses, indent=4))
+
 
 if __name__ == "__main__":
     test()
